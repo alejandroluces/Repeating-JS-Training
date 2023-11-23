04c2984aacdaa0fd697c45615769fe4e3cd7f9f8
Alejandro Luces González <aluces@polytex.local>
2023-11-23 18:31:25 -0300

    Add_codewars_44

---
+++ Ej_codewards_44.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/57126304cdbf63c6770012bd/train/javascript
+let input1 = "1-1"
+let input2 = "  3 5"
+let input3 = " zero"
+let input4 = "s2324"
+let input5 = "-234.4"
+
+
+
+
+const isDigit =(s) => {
+   return +s===parseInt(s)
+
+}
+
+console.log(isDigit(input1));
+console.log(isDigit(input2));
+console.log(isDigit(input3));
+console.log(isDigit(input4));
+console.log(isDigit(input5));
+
+
+
+
+
+
